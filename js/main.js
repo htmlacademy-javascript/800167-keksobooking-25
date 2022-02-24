52d4de6bf71c9a2b916cc4cd029e1a212efd06e4
Ruslan <Snow88ilike@gmail.com>
2022-02-25 00:26:15 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-function getInteger(min, max) {
+function getInteger(min = 0, max = 10) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if ( min >= 0 && min <= max ) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  if ( min < 0 || min >= max ) {
+    throw new Error(`Значение min: ${min} превышает значение max: ${max} или указано отрицательное число`);
   }
-  // eslint-disable-next-line no-alert
-  alert(`Значение min: ${min} превышает значение max: ${max} или указано отрицательное число`);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getInteger(8,25);
+getInteger(14,25);
 
 
-function randomNumWithDecimal(min, max, numberOfDecimal) {
+function randomNumWithDecimal(min = 0, max = 10, numberOfDecimal = 1) {
 
-  if ( min >= 0 && min <= max ) {
-    return Number(Math.floor(Math.random() * (max - min + 1)) + min).toFixed(numberOfDecimal);
+  if ( min < 0 || min >= max ) {
+    throw new Error(`Значение min: ${min} превышает значение max: ${max} или указано отрицательное число`);
   }
-  // eslint-disable-next-line no-alert
-  alert(`Значение min: ${min} превышает значение max: ${max} или указано отрицательное число`);
+  return Number(Math.floor(Math.random() * (max - min + 1)) + min).toFixed(numberOfDecimal);
 }
 
-randomNumWithDecimal(45.8344444,89.888777, 3);
+randomNumWithDecimal(45.8344444, 89.888777, 3);
