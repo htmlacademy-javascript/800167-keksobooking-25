fd75c390e3438ab2e4e2f691bf1dd37c3dfe1098
Ruslan <Snow88ilike@gmail.com>
2022-03-14 15:44:14 +0300

    connect func 2 in 1

--- js/form.js
+++ js/form.js
@@ -4,20 +4,19 @@ const mapForm = document.querySelector('.map__filters');
 const mapFormElements = Array.from(mapForm.children);
 
 
-const toggleStateInactive = () => {
-  adForm.classList.add('ad-form--disabled');
-  mapForm.classList.add('map__filters--disabled');
-  adFormElements.forEach((elem) => elem.setAttribute('disabled', true));
-  mapFormElements.forEach((elem) => elem.setAttribute('disabled', true));
+const toggleUiState = (isActive) => {
+  if (isActive) {
+    adForm.classList.remove('ad-form--disabled');
+    mapForm.classList.remove('map__filters--disabled');
+    adFormElements.forEach((elem) => elem.removeAttribute('disabled'));
+    mapFormElements.forEach((elem) => elem.removeAttribute('disabled'));
+  } else {
+    adForm.classList.add('ad-form--disabled');
+    mapForm.classList.add('map__filters--disabled');
+    adFormElements.forEach((elem) => elem.setAttribute('disabled', true));
+    mapFormElements.forEach((elem) => elem.setAttribute('disabled', true));
+  }
 };
 
 
-const toggleStateActive = () => {
-  adForm.classList.remove('ad-form--disabled');
-  mapForm.classList.remove('map__filters--disabled');
-  adFormElements.forEach((elem) => elem.setAttribute('disabled', false));
-  mapFormElements.forEach((elem) => elem.setAttribute('disabled', false));
-};
-
-
-export { toggleStateInactive, toggleStateActive };
+export { toggleUiState };
