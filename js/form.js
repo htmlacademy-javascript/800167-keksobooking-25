4c87dca18547a00804e0e0f61f2d6f297c97d978
Ruslan <Snow88ilike@gmail.com>
2022-04-08 15:02:53 +0300

    Фото на память

--- js/form.js
+++ js/form.js
@@ -5,12 +5,17 @@ import { showErrorMessage, showTypicalMessage } from './alerts.js';
 import { DICTIONARY } from './dictionary.js';
 import { activateFilters, toggleFiltersStateUi } from './filters.js';
 import { debounce } from './utils.js';
+import { previewPicture } from './preview.js';
 
 const adForm = document.querySelector('.ad-form');
 const adFormElements = Array.from(adForm.children);
 const mapForm = document.querySelector('.map__filters');
 const adFormSendButton = document.querySelector('.ad-form__submit');
 const adFormResetButton = document.querySelector('.ad-form__reset');
+const uploadAvatarInput = document.querySelector('.ad-form-header__input');
+const formAvatarField = document.querySelector('.ad-form-header__avatar');
+const uploadPhotoInput = document.querySelector('.ad-form__input');
+const formPhotoField = document.querySelector('.ad-form__photo');
 
 const RESET_PIN_DELAY = 100;
 const RERENDER_DELAY = 500;
@@ -83,5 +88,8 @@ adForm.addEventListener('submit', (evt) => {
     );
   }
 });
+
+previewPicture(uploadAvatarInput, formAvatarField);
+previewPicture(uploadPhotoInput, formPhotoField, true);
 
 export { toggleUiState };
